a7341f15b9bea2bd74b2c5249918466565079f83
Richard Khims <khims.richard@gmail.com>
2020-01-07 03:32:05 +0400

    add empty StudentServlet

---
+++ Itschool/src/main/java/org/teamshare/itschool/servlets/StudentServlet.java
@@ -0,0 +1,39 @@
+package org.teamshare.itschool.servlets;
+
+import org.teamshare.itschool.services.StudentService;
+import org.teamshare.itschool.services.qualifiers.StudentServiceQualifier;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/students")
+public class StudentServlet extends HttpServlet {
+    @Inject
+    @StudentServiceQualifier
+    private StudentService studentService;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
+}
